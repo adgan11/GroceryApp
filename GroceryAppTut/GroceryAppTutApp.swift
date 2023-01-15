eebc40c7fd956c800f3d9d2109ecd9f535c2f20e
Ahmed Gagan <67264609+adgan11@users.noreply.github.com>
2023-01-15 12:56:11 +0530

    Initial Commit

---
+++ GroceryAppTut/GroceryAppTutApp.swift
@@ -0,0 +1,17 @@
+//
+//  GroceryAppTutApp.swift
+//  GroceryAppTut
+//
+//  Created by Ahmed Gagan on 15/01/23.
+//
+
+import SwiftUI
+
+@main
+struct GroceryAppTutApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
